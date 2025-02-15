18cca14b482dc1bdd8541634ab11d7097e037363
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2025-02-15 14:22:08 +0700

    Remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ExpensesController } from './controllers/expenses/expenses.controller';
-
+import { ExpenseModule } from './expense/expense.module';
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController, ExpensesController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), ExpenseModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
--- src/controllers/expenses/expenses.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('expenses')
-export class ExpensesController {
-    @Get()
-    findAll(): string {
-        return 'Test return';
-    }
-}
